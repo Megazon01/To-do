cc40aeede5b91ae0f9372c804c698cb9db9e6dbe
Megazon <pzonias@hotmail.com>
2023-12-06 21:48:10 +0000

    Edit Button Functionality

--- gui.py
+++ gui.py
@@ -8,9 +8,13 @@ add_button = sg.Button("Add")
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 show_button = sg.Button("Show")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
 
 Window = sg.Window("My To-do App",
-                   layout=[[label, input_box], [add_button, edit_button, complete_button, show_button]],
+                   layout=[[label, input_box],
+                           [add_button, complete_button, show_button],
+                           [list_box, edit_button]],
                    font=("Helvetica", 20))
 
 
@@ -21,9 +25,23 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todo"] + '\n'
+            new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            Window["todos"].update(values=todos)
+
+        case "Edit":
+            old_todo = values["todos"][0]
+            new_todo = values["todo"] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(old_todo)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            Window["todos"].update(values=todos)
+
+        #case "todos":
+        #    Window["todo"].update(value=values["todos"][0])
+
 
         case sg.WIN_CLOSED:
             break
